21af2c136e8d1ef656e7fd5e90078ccfcd6d196e
Matt young <matthew.young@bisonworks.com>
2019-04-19 15:50:06 -0700

    cleanup

--- run.sh
+++
@@ -1,7 +0,0 @@
-java -jar lib/sonar-application-7.6.jar \
-  -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
-  "$@"
